5e8b0e704b6c90a3f6241f6fa480e1612afe80da
jeanvaes <jeanvaes@unisabana.edu.co>
2023-09-16 18:25:31 -0500

    Agregar archivos tests
    Agregar archivos tests de controllers y logica

---
+++ src/test/java/co/edu/unisabana/puntosUnisabana/integracion/ClienteControllerTest.java
@@ -0,0 +1,8 @@
+package co.edu.unisabana.puntosUnisabana.integracion;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ClienteControllerTest {
+}
---
+++ src/test/java/co/edu/unisabana/puntosUnisabana/unit/logica/TransaccionLogicaTest.java
@@ -0,0 +1,4 @@
+package co.edu.unisabana.puntosUnisabana.unit.logica;
+
+public class TransaccionLogicaTest {
+}
