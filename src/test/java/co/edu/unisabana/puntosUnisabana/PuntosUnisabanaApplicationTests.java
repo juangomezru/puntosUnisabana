ea36407a56d2a4a367f0cbeb544c96fd5663a86d
jeanvaes <jeanvaes@unisabana.edu.co>
2023-10-23 08:37:56 -0500

    resolver conexión https
    agregar certificado, keystore y solucionar perfil de tests

--- src/test/java/co/edu/unisabana/puntosUnisabana/PuntosUnisabanaApplicationTests.java
+++ src/test/java/co/edu/unisabana/puntosUnisabana/PuntosUnisabanaApplicationTests.java
@@ -2,8 +2,10 @@ package co.edu.unisabana.puntosUnisabana;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class PuntosUnisabanaApplicationTests {
 
 	@Test
