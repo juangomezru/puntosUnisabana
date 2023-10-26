c877bddefcf6d0c7b169caeda3da5a34e1590def
jeanvaes <jeanvaes@unisabana.edu.co>
2023-10-25 22:35:40 -0500

    Corregir seguridad
    Agregar configuración de endpoints a la seguridad de autenticación

---
+++ src/main/java/co/edu/unisabana/puntosUnisabana/configuration/SecutiryConfigApp.java
@@ -0,0 +1,38 @@
+package co.edu.unisabana.puntosUnisabana.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfigApp extends WebSecurityConfigurerAdapter{
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+}
